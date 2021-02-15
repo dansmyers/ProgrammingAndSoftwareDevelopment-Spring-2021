6ea21483c2639a7fc33f190de87e1c5e5112a24c
Dan S. Myers <dmyers@rollins.edu>
2021-02-15 11:32:11 -0500

    Update DieRoll.java

--- 3-Methods_and_Recursion/Examples/DieRoll.java
+++ 3-Methods_and_Recursion/Examples/DieRoll.java
@@ -1,21 +1,41 @@
-// Use a method to generate and return a die roll
-// CMS 167
+/**
+ * Examples of rolling dice
+ * 
+ * Key idea: Java uses parameters to distinguish between multiple methods with the same name
+ * 
+ * @author dmyers
+ *
+ */
 
 public class DieRoll {
 
-    // Generate and return a random die roll
-    // The return type is int
-    public static int roll() {
-        int die = (int) (Math.random() * 6 + 1);
-        return die;
-    }
+	/**
+	 * Simulate the roll of a six-sided die
+     * Wrapper method for the more general dieRoll(n)
+	 * 
+	 * @return the randomly generated die roll
+	 */
+	public static int dieRoll() {
+		return dieRoll(6);
+	}
 
-    public static void main(String[] args) { 
-        int die1 = roll();
-        int die2 = roll();
-        int sum = die1 + die2;
-        
-        System.out.printf("The two dice are %d and %d. The sum is %d.", die1, die2, sum);  
-    }
-    
+	/**
+	 * Parameterized die roll that takes the number of faces as input
+	 * 
+	 * @param faces the number of faces on the die
+	 * @return the simulated roll from the range [1, faces]
+	 */
+	public static int dieRoll(int faces) {
+		return (int) (Math.random() * faces + 1);
+	}
+
+	public static void main(String[] args) {
+
+		// Generate some die rolls
+		int d20 = dieRoll(20);
+		int d100 = dieRoll(100);
+		int d6 = dieRoll();
+
+	}
+
 }
