77b8bc43af191d5e80ff9f874ba1eb2ea7838dbf
Dan S. Myers <dmyers@rollins.edu>
2021-03-01 15:23:09 -0500

    Update Card.java

--- 4-Classes_and_Objects/Deliverables/Blackjack/Card.java
+++ 4-Classes_and_Objects/Deliverables/Blackjack/Card.java
@@ -10,43 +10,43 @@
 // Enumerated suit and rank types
 
 enum Suit {
-	CLUBS, DIAMONDS, HEARTS, SPADES;
+    CLUBS, DIAMONDS, HEARTS, SPADES;
 }
 
 enum Rank {
-	ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING;
+    ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING;
 }
 
 
 public class Card {
 
-	  private Suit suit;
-	  private Rank rank;
-	
-	  /**
-	   * Constructor -- create a new card with a specified Suit and Rank
-	   */
-	  public Card(Suit newSuit, Rank newRank) {
-		    this.suit = newSuit;
-		    this.rank = newRank;
-	  }
-	
-	  /**
-	   * Get methods -- return the value of suit and rank
-	   */
-	  public Suit getSuit() {
-		    return this.suit;
-	  }
-	
-	  public Rank getRank() {
-		    return this.rank;
-	  }
-	
-	  /**
-	   * toString -- return a String reprsentation of the object
-	   */
-	  public String toString() {
-		    return this.rank + " of " + this.suit;
-	  }
+    private Suit suit;
+    private Rank rank;
+
+    /**
+     * Constructor -- create a new card with a specified Suit and Rank
+     */
+    public Card(Suit newSuit, Rank newRank) {
+        this.suit = newSuit;
+        this.rank = newRank;
+    }
+
+    /**
+     * Get methods -- return the value of suit and rank
+     */
+    public Suit getSuit() {
+        return this.suit;
+    }
+
+    public Rank getRank() {
+        return this.rank;
+    }
+
+    /**
+     * toString -- return a String reprsentation of the object
+     */
+    public String toString() {
+        return this.rank + " of " + this.suit;
+    }
 	
 }
