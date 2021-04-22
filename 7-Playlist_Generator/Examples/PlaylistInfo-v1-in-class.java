a662d922adc94b1915f612c3e546b39fd82ccf89
Dan S. Myers <dmyers@rollins.edu>
2021-04-22 09:54:55 -0400

    Create PlaylistInfo-v1-in-class.java

---
+++ 7-Playlist_Generator/Examples/PlaylistInfo-v1-in-class.java
@@ -0,0 +1,52 @@
+/**
+ * Class representing information about a playlist
+ * 
+ */
+ 
+ package cms121_playlist_generator;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class PlaylistInfo {
+	
+	private String id;  // Unique id string for this playlist (this will be the same as the filename)
+	private ArrayList<String> tracks;  // List of track IDs for this playlist
+	
+	
+	public PlaylistInfo(String id, String title) {
+		this.id = id;
+		this.tracks = new ArrayList<String>();
+	}
+	
+	
+	public String getTitle() {
+	    return this.title;
+	}
+		
+	
+	public void addTrack(String newTrackId) {
+        this.tracks.add(newTrackId);
+	}
+	
+	
+	public ArrayList<String> getTracks() {
+	    return this.tracks;
+	}
+	
+	
+	/**
+	 * Return the id of a randomly chosen track
+	 * @return
+	 */
+	public String randomTrack() {
+        Random r = new Random();
+        int index = r.nextInt(this.tracks.size());
+        return this.tracks.get(index);
+	}
+	
+	
+	public String toString() {
+		return this.tracks.toString();
+	}
+}
